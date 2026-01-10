54efb42eab6b14f6b8feb9c0b7fe8b22d476009f
nha4110 <139923510+nha4110@users.noreply.github.com>
2026-01-10 12:27:30 +0700

    new file:   Lab1/myphp.php

---
+++ Lab1/myphp.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <title>My First PHP webpage</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="web development">
+    <meta name="keyword" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Lu Nhat Hoang - 105234956">
+</head>
+<body>
+    <h1>Web Programming - Lab 1</h1>
+    <p>Can I create a .php file that do not contain any PHP scripts?</p>
+    <p>Yes, but this should be avoided, so that the server does not do any unnecessary parsing and processing.</p> 
+</body>
+</html>

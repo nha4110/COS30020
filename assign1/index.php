a05fbc8805264e2460d90920ac7cccdb6e8fed57
nha4110 <139923510+nha4110@users.noreply.github.com>
2026-01-11 16:53:36 +0700

    assign1
    proto

---
+++ assign1/index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index page for the job search sys</title>
+</head>
+<body>
+    <?php
+
+    ?>
+    <h1>Job posting website.</h1>
+    <p>Name: Lu Nhat Hoang</p>
+    <p>Student ID: 105234956</p>
+    <p>Emai: <a href="mailto:105234956@student.swin.edu.au">105234956@student.swin.edu.au</a></p>
+    <p>bruh bruh bruh</p>
+</body>
+</html>

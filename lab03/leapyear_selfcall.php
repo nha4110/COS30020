18e43c8c80f5213fa60441f8ebac2e468a9d7206
nha4110 <139923510+nha4110@users.noreply.github.com>
2026-01-14 17:51:19 +0700

    lab03 done

---
+++ lab03/leapyear_selfcall.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html> 
+<html lang="en" lang="en" > 
+<head> 
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+    <meta name="description" content="Web Application Development :: Lab 3" /> 
+    <meta name="keywords" content="Web,programming" /> 
+    <title>leap year form</title> 
+</head> 
+<body> 
+    <h1>Lab 03 Task 2 - Leap Year</h1>
+    <form action="leapyear_selfcall.php" method="get">
+        <label for="leap_cal">Year:</label>
+        <input type="text" id="leap_cal" name="leap_cal" required>
+        <button type="submit">Check For Leap Year</button>
+    </form>
+    <?php
+        function is_leapyear ($year) {
+            return ($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0);
+        }
+        // leap year calculation 
+        if (!isset ($_GET['leap_cal'])) {
+            exit;
+        } 
+        $y = $_GET['leap_cal'];
+        if (!is_numeric($y) || $y <= 0) { // check if its a number
+            echo "<p>Please retry and enter a year in integer form.</p>";
+            exit;
+        }
+        if (is_leapyear($y)) {
+            echo "<p>The year you entered $y is a leap year.</p>";
+        }
+        else {
+            echo "<p>The year you entered $y is not a leap year.</p>";
+        }        
+    ?>
+</body> 
+</html> 
--- lab03/primenumber.php
+++ lab03/primenumber.php
@@ -7,17 +7,28 @@
     <title>prime number calculation</title> 
 </head> 
 <body> 
+    <h1>Lab 03 Task 3 - Prime Number</h1>
     <?php 
         // prime number can only be divided by itself or by 1. 
         if (!isset($_GET['Pri_num'])) {
-            echo "Invalid input"; // get input
+            echo "<p>Invalid input</p>";
             exit;
         }
         $prime = $_GET['Pri_num'];
-        $isprime = true;
+        function is_prime($P) {
+            // Don't have to check every number just have to check every integer below the squa root of the num
+            $count = sqrt($P); 
+            // Will be the number of loop and number of integer we need to divide
+            for ($i = 2; $i <= $count ; $i++) {
+                if ($P % $i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
 
         if (!is_numeric($prime)) {
-            echo "Invalid input"; 
+            echo "<p>Invalid input</p>"; 
             exit; 
         }
         // Make sure it's <= 1 are not prime
@@ -26,16 +37,7 @@
             // 1 is not a prime number but in lab03 ex have it so i don't know if i should include it.
             exit;
         }
-        // Don't have to check every number just have to check every integer below the squa root of the num
-        $count = sqrt($prime); 
-        // Will be the number of loop and number of integer we need to divide
-        for ($i = 2; $i <= $count ; $i++) {
-            if ($prime % $i == 0) {
-                $isprime = false;
-                break;
-            }
-        }
-        if ($isprime) {
+        if (is_prime($prime)) {
             echo "<p>The number you enter $prime is a prime number</p>";
         } else {
             echo "<p>The number you enter $prime is not a prime number</p>";

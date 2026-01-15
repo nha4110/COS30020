d4e879800c5ff370225da444cf6c1472c3ec163d
nha4110 <139923510+nha4110@users.noreply.github.com>
2026-01-15 18:32:48 +0700

    lab 02 update done

--- lab02/daysarray.php
+++ lab02/daysarray.php
@@ -10,25 +10,14 @@
     <!-- display days in english and french-->
      <?php
         $days = array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-        $size = count($days);
+        
         echo "<p>The days of the week in English are:</p>";
         // for loop to loop throught the array if statement to make sure the last one is a "."
-        for ($x = 0; $x < $size; $x++) {
-            if ($x == $size -1) {
-                echo "$days[$x].";
-            }
-            else echo "$days[$x], ";
-        }
+        echo implode(", ", $days) . ".";
         // step 3 reassign the values of days
         $days = array ("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
         echo "<p>The days of the week in French are: </p>";
-        for ($x = 0; $x < $size; $x++) {
-            if ($x == $size -1) {
-                echo "$days[$x].";
-            }
-            else echo "$days[$x], ";
-        }
+        echo implode(", ", $days) . ".";
      ?>
-     
 </body>
 </html>
